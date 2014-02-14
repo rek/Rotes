3bb48240302f351727f3ae6f85a2dd366bbeb62c
rekarnar <rekarnar@gmail.com>
2014-02-14 23:25:15 +0530

    tidyup of user bundle

--- src/REK/RotesBundle/Tests/Controller/DefaultControllerTest.php
+++ src/REK/RotesBundle/Tests/Controller/DefaultControllerTest.php
@@ -13,5 +13,24 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/hello/Fabien');
 
         $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
-    }
+
+// $link = $crawler->filter('a:contains("Greet")')->eq(1)->link();
+
+// $crawler = $client->click($link);
+
+// $form = $crawler->selectButton('submit')->form();
+// set some values
+// $form['name'] = 'Lucas';
+// $form['form_name[subject]'] = 'Hey there!';
+// submit the form
+// $crawler = $client->submit($form);
+
+// Assert that the response matches a given CSS selector.
+// $this->assertGreaterThan(0, $crawler->filter('h1')->count());    }
+
+// $this->assertRegExp(
+    // '/Hello Fabien/',
+    // $client->getResponse()->getContent()
+// );
+
 }
