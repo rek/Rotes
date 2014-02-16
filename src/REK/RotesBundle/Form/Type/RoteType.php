59cdbec8ec24072e9a69b142bf71a8ad246c6269
rekarnar <rekarnar@gmail.com>
2014-02-16 12:30:50 +0530

    form and its service def

---
+++ src/REK/RotesBundle/Form/Type/RoteType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace REK\RotesBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class RoteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('message', 'textarea')
+            ->add('save', 'submit');
+
+            // 'options' => array('translation_domain' => 'FOSUserBundle'),
+    }
+
+    // for binding to the correct model
+    // required with using embedded forms
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'REK\RotesBundle\Model\Rote',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'rote';
+    }
+}
