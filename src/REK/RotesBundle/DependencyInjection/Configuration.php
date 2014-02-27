db9cbe43522864cb86d7091913243ffe296480b9
rekarnar <rekarnar@gmail.com>
2014-02-27 12:26:31 +0530

    adding some config for rotes

--- src/REK/RotesBundle/DependencyInjection/Configuration.php
+++ src/REK/RotesBundle/DependencyInjection/Configuration.php
@@ -20,9 +20,13 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('rek_rotes');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->booleanNode('disable_registration')
+                    ->defaultFalse()
+                ->end()
+            ->end()
+        ;
 
         return $treeBuilder;
     }
--- src/REK/RotesBundle/DependencyInjection/REKRotesExtension.php
+++ src/REK/RotesBundle/DependencyInjection/REKRotesExtension.php
@@ -5,7 +5,9 @@ namespace REK\RotesBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -27,6 +29,6 @@ class REKRotesExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-        // $loader->load('doctrine_extensions.yml');
+        // $loader->load('config.yml');
     }
 }
